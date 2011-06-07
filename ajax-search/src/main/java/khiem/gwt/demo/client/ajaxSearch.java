190875218e4da42524aba9476180d6e8b9fed2e3
dohoangkhiem <dohoangkhiem@gmail.com>
2011-06-07 09:04:30 +0000

    [ajax-search] add some code, remove useless config

--- ajax-search/src/main/java/khiem/gwt/demo/client/ajaxSearch.java
+++ ajax-search/src/main/java/khiem/gwt/demo/client/ajaxSearch.java
@@ -1,6 +1,13 @@
 package khiem.gwt.demo.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.search.client.ExpandMode;
+import com.google.gwt.search.client.ImageSearch;
+import com.google.gwt.search.client.ResultSetSize;
+import com.google.gwt.search.client.SearchControl;
+import com.google.gwt.search.client.SearchControlOptions;
+import com.google.gwt.search.client.WebSearch;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -12,6 +19,14 @@ public class ajaxSearch implements EntryPoint {
    * This is the entry point method.
    */
   public void onModuleLoad() {
-    
+    SearchControlOptions options = new SearchControlOptions();
+    WebSearch webSearch = new WebSearch();
+    webSearch.setResultSetSize(ResultSetSize.LARGE);
+    options.add(webSearch);
+    ImageSearch imageSearch = new ImageSearch();
+    options.add(imageSearch, ExpandMode.OPEN);
+    final SearchControl control = new SearchControl(options);
+    control.execute("treehouse");
+    RootPanel.get().add(control);
   }
 }
